3a75f6479e40806aeb692163a0ca1630d3d7b0cd
iqbal <iqbalitfadil@gmail.com>
2021-02-09 23:40:02 +0700

    Slicing Hero pt 1

--- fe-app/src/pages/LandingPage.js
+++ fe-app/src/pages/LandingPage.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Header from '../parts/Header';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
+import landingPage from '../json/landingPage.json'; 
+import Hero from '../parts/Hero';
 
 
 export default class LandingPage extends Component{
@@ -9,7 +11,9 @@ export default class LandingPage extends Component{
         return(
             <>
                 <Header {...this.props}></Header>
+                <Hero data={landingPage.hero}/>
+                
             </>
-        )
+        );
     }
 }
