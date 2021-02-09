47e72daf55e3bcbf4f84f115218f57c258650831
m_iqbal <iqbalitfadil@gmail.com>
2021-02-09 16:50:56 +0700

    Slicing add Header

--- fe-app/src/App.js
+++ fe-app/src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import './assets/scss/style.scss'
+import "./assets/scss/style.scss";
 import LandingPage from "./pages/LandingPage";
 
 function App() {
-  return
+  return(
   <div className="App">
     <Router>
-      <Route path="/" component={LandingPage}></Route>
+      <Route path="/" component={LandingPage}/>
     </Router>
   </div>
+  );
 }
 
 export default App;
--- fe-app/src/pages/LandingPage.js
+++ fe-app/src/pages/LandingPage.js
@@ -1,5 +1,8 @@
-import React, { Component } from 'react'
-import Header from '../parts/Header'
+import React, { Component } from 'react';
+import Header from '../parts/Header';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
+
 
 export default class LandingPage extends Component{
     render(){
